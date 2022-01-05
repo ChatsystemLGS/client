bf558802c5e978b3dbf55b552dc68b71273004ec
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-05 20:01:51 +0100

    added constructor

--- client-app/src/main/java/client/Session.java
+++ client-app/src/main/java/client/Session.java
@@ -2,6 +2,18 @@ package client;
 
 public class Session {
 
-    public Session(Server client) {
-    }
+    private final Client client;
+
+	private State state;
+	private User user;
+
+	public Session(Client client) {
+		this.client = client;
+		state = State.CONNECTED;
+	}
+
+    public String greet() {
+		return response(Status.OK, PROTOCOL_VERSION);
+	}
+    
 }
