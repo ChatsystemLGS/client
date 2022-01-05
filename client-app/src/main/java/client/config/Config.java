ecd59935eef62cfb4d5cc5fcb0c67d6c0e6ff613
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-05 20:02:30 +0100

    config with port & server IP

--- client-app/src/main/java/client/config/Config.java
+++ client-app/src/main/java/client/config/Config.java
@@ -1,17 +1,48 @@
 package client.config;
 
+import java.io.File;
+
 public class Config {
 
-    public final int MAX_MESSAGE_LENGTH;
-    public final String ADDRESS;
-    public final int PORT;
+	public final int MAX_MESSAGE_LENGTH;
+	public final int PORT;
+	public final String ADDRESS;
 
-    public static final Config DEFAULT_CONFIG = new Config(Integer.MAX_VALUE, "localhost", 1465);
+	public static final Config DEFAULT_CONFIG = new Config(Integer.MAX_VALUE, 1465, "localhost");
 
-    public Config(int maxMessageLength, String address, int port) {
+	public Config(int maxMessageLength, int port, String address) {
 
-        MAX_MESSAGE_LENGTH = maxMessageLength;
-        this.ADDRESS = address;
-        this.PORT = port;
-    }
+		MAX_MESSAGE_LENGTH = maxMessageLength;
+		this.PORT = port;
+		this.ADDRESS = address;
+
+	}
+
+	public static Config createFromArgs(String[] args, Config defaultValues) {
+
+		ArgMap parameters = new ArgMap(args);
+
+		int port;
+		String address;
+
+		address = parameters.getString("address", defaultValues.ADDRESS);
+
+		try {
+			port = parameters.getInteger("port", defaultValues.PORT);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
+
+		return new Config(DEFAULT_CONFIG.MAX_MESSAGE_LENGTH, port, address);
+	}
+
+	public static Config loadCfg(File f) {
+		// TODO
+		return null;
+	}
+
+	public static void WriteCfg(File f, Config cfg) {
+		// TODO
+	}
+
 }
