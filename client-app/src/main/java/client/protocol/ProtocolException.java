d1df1c4ed889e899bae6ed97a3f80d343e94114c
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-05 20:05:12 +0100

    removed unnecessary, implement later

--- client-app/src/main/java/client/protocol/ProtocolException.java
+++ client-app/src/main/java/client/protocol/ProtocolException.java
@@ -2,17 +2,4 @@ package client.protocol;
 
 public abstract class ProtocolException extends Exception {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = -2162828148268985546L;
-    private final Status status;
-
-    ProtocolException(Status status) {
-        this.status = status;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
 }
