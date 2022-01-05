39abe7fd1e7ae5b1a032c91bf144f8d8d98ea96f
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-05 20:01:15 +0100

    establish connection and read/write

--- client-app/src/main/java/client/ServerHandler.java
+++ client-app/src/main/java/client/ServerHandler.java
@@ -5,25 +5,58 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+import server.simplelogger.SimpleLogger;
+import server.simplelogger.SimpleLogger.LogLevel;
+
 public class ServerHandler implements Runnable {
 
-    Socket socket;
+    Socket server;
     Scanner s;
     PrintWriter pw;
 
     Session session;
 
-    private ServerHandler(Socket client, Server server) throws IOException {
-        this.socket = client;
+    private ServerHandler(Socket server, Client client) throws IOException {
+        this.server = server;
 
-        s = new Scanner(client.getInputStream());
-        pw = new PrintWriter(client.getOutputStream(), true);
+        s = new Scanner(server.getInputStream());
+        pw = new PrintWriter(server.getOutputStream(), true);
 
-        session = new Session(server);
+        // session = new Session(client);
     }
 
     @Override
     public void run() {
+        try {
+            // TODO: implement sending commands to the server
+            writeLine(session.greet());
+            while (session.getState() != Session.State.DISCONNECTED) {
+                readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+                System.out.println("disconnected from server");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private String readLine() {
+		String line = s.nextLine();
+		SimpleLogger.logf(LogLevel.DEBUG, "server [%s]:%s > %s", line);
+		return line;
+	}
+
+	private void writeLine(String line) {
+		SimpleLogger.logf(LogLevel.DEBUG, "server [%s]:%s < %s", line);
+		pw.println(line);
+	}
+
+    public static void createClient(Socket server, Client client) throws IOException {
+        new ServerHandler(server, client);
     }
 }
