8d45ea5d33a695c1cbcfd57f8df030f84b0cd154
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-09 10:25:25 +0100

    added log

--- client-app/src/main/java/client/App.java
+++ client-app/src/main/java/client/App.java
@@ -1,20 +1,41 @@
 package client;
 
+import java.io.File;
 import java.io.IOException;
 
 import client.config.Config;
+import client.simplelogger.ConsoleLogger;
+import client.simplelogger.FileLogger;
+import client.simplelogger.FileLogger.LogType;
+import client.simplelogger.SimpleLogger;
+import client.simplelogger.SimpleLogger.LogLevel;
 
 public class App {
 
-    private Client client;
+  private Client client;
 
-    public static void main(String[] args) throws IOException {
-		  new App(Config.createFromArgs(args, Config.DEFAULT_CONFIG));
-    }
+  public static void main(String[] args) {
 
-    private App(Config cfg) throws IOException {
+    // setup logging
+		try {
+			FileLogger fileLogger = new FileLogger(new File("./latest.log"), LogType.APPEND, LogLevel.DEBUG);
+			SimpleLogger.addLogListener(fileLogger);
 
-		client = new Client(cfg);
+			ConsoleLogger consoleLogger = new ConsoleLogger(LogLevel.DEBUG);
+			SimpleLogger.addLogListener(consoleLogger);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	}
+    new App(Config.createFromArgs(args, Config.DEFAULT_CONFIG));
+    
+  }
+
+  private App(Config cfg) {
+
+    client = new Client(cfg);
+
+  }
 }
