848f93043e965b1173a4009896808cf1c8fe1309
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-09 10:27:48 +0100

    added file

---
+++ client-app/src/main/java/client/db/Channel.java
@@ -0,0 +1,44 @@
+package client.db;
+
+public class Channel extends TransmittableObject {
+
+	private Attr<Integer> id = new Attr<>();
+	private Attr<ChannelType> type = new Attr<>();
+	private Attr<String> name = new Attr<>();
+	
+	public Channel() {
+		registerAttributes(id, type, name);
+	}
+	
+	public Channel withId(int id) {
+		this.id.set(id);
+		return this;
+	}
+
+	public Channel withType(ChannelType type) {
+		this.type.set(type);
+		return this;
+	}
+
+	public Channel withName(String name) {
+		this.name.set(name);
+		return this;
+	}
+
+	public int getId() {
+		return id.getValue();
+	}
+
+	public ChannelType getType() {
+		return type.getValue();
+	}
+
+	public String getName() {
+		return name.getValue();
+	}
+
+	public enum ChannelType {
+		DM, PUBLIC_GROUP, PRIVATE_GROUP
+	}
+
+}
---
+++ client-app/src/main/java/client/db/TransmittableObject.java
@@ -0,0 +1,74 @@
+package client.db;
+
+import java.util.Arrays;
+import java.util.Base64;
+
+public abstract class TransmittableObject {
+
+	// use hashmap instead of array?
+	private Attr<?>[] attributes = new Attr<?>[0];
+
+	// should be called to register all attributes that should be included in
+	// generated String
+	protected void registerAttributes(Attr<?>... attributes) {
+		this.attributes = attributes;
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(attributes);
+		
+//		String s = "";
+//
+//		for (int i = 0; i < attributes.length - 1; i++) {
+//			s += attributes[i].key() + " " + attributes[i].toString() + " ";
+//		}
+//		s += attributes[attributes.length - 1].toString();
+//
+//		return s;
+	}
+	
+	public String transmittableString() {
+		Attr<?>[] filteredAttributes = Arrays.stream(attributes).filter(a -> a.isSet()).toArray(Attr<?>[]::new);
+
+		String s = "";
+
+		for (int i = 0; i < filteredAttributes.length - 1; i++) {
+			
+			
+			
+			s += filteredAttributes[i].transmittableString() + " ";
+		}
+		s += filteredAttributes[filteredAttributes.length - 1].transmittableString();
+
+		return s;
+	}
+
+	public static String toBase64String(byte[] data) {
+		return Base64.getEncoder().encodeToString(data);
+	}
+
+	public static byte[] fromBase64String(String data) {
+		return Base64.getDecoder().decode((data).getBytes());
+	}
+	
+	// borrowed from Arrays.toString(Object[] o)
+	public static String toString(TransmittableObject[] a) {
+		if (a == null)
+			return "null";
+
+		int iMax = a.length - 1;
+		if (iMax == -1)
+			return "[]";
+
+		StringBuilder b = new StringBuilder();
+		b.append('[');
+		for (int i = 0;; i++) {
+			b.append(a[i].transmittableString());
+			if (i == iMax)
+				return b.append(']').toString();
+			b.append(", ");
+		}
+	}
+
+}
