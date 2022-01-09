3675358bc06177c41b0599768e4d423419c26a83
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-09 01:29:46 +0100

    format

--- client-app/src/main/java/client/App.java
+++ client-app/src/main/java/client/App.java
@@ -9,7 +9,7 @@ public class App {
     private Client client;
 
     public static void main(String[] args) throws IOException {
-		new App(Config.createFromArgs(args, Config.DEFAULT_CONFIG));
+		  new App(Config.createFromArgs(args, Config.DEFAULT_CONFIG));
     }
 
     private App(Config cfg) throws IOException {
