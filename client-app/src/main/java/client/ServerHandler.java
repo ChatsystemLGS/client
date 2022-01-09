4a08d05f581afff73ee35a44a86a54929709562a
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-09 10:26:57 +0100

    fix errors

--- client-app/src/main/java/client/ServerHandler.java
+++ client-app/src/main/java/client/ServerHandler.java
@@ -3,10 +3,11 @@ package client;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import server.simplelogger.SimpleLogger;
-import server.simplelogger.SimpleLogger.LogLevel;
+import client.simplelogger.SimpleLogger;
+import client.simplelogger.SimpleLogger.LogLevel;
 
 public class ServerHandler implements Runnable {
 
@@ -22,23 +23,30 @@ public class ServerHandler implements Runnable {
         s = new Scanner(server.getInputStream());
         pw = new PrintWriter(server.getOutputStream(), true);
 
-        // session = new Session(client);
+        session = new Session(client);
+
+        SimpleLogger.logf(LogLevel.DEBUG, "server: %s", session.getState());
     }
 
     @Override
     public void run() {
         try {
             // TODO: implement sending commands to the server
-            writeLine(session.greet());
+
             while (session.getState() != Session.State.DISCONNECTED) {
-                readLine();
+
+                if (readLine().equalsIgnoreCase("INVALID_PARAMETER 1")) {
+                    session.disconnect();
+                } else {
+                    writeLine("LOGIN mirko.leon.weih@lgs-hu.eu mWe1h_1234");
+                }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NoSuchElementException e) {
+            session.disconnect();
         } finally {
             try {
                 server.close();
-                System.out.println("disconnected from server");
+                SimpleLogger.logf(LogLevel.DEBUG, "client: %s", session.getState());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -47,16 +55,16 @@ public class ServerHandler implements Runnable {
 
     private String readLine() {
 		String line = s.nextLine();
-		SimpleLogger.logf(LogLevel.DEBUG, "server [%s]:%s > %s", line);
+		SimpleLogger.logf(LogLevel.DEBUG, "server: > %s", line);
 		return line;
 	}
 
 	private void writeLine(String line) {
-		SimpleLogger.logf(LogLevel.DEBUG, "server [%s]:%s < %s", line);
+		SimpleLogger.logf(LogLevel.DEBUG, "server: < %s", line);
 		pw.println(line);
 	}
 
     public static void createClient(Socket server, Client client) throws IOException {
-        new ServerHandler(server, client);
-    }
+		new Thread(new ServerHandler(server, client)).start();
+	}
 }
