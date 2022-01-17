aa69910c60c5795e555d301d5c0fe28a861c8e45
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-17 09:35:01 +0100

    bug fixes

--- src/main/java/client/ServerHandler.java
+++ src/main/java/client/ServerHandler.java
@@ -3,12 +3,18 @@ package client;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.Timestamp;
+import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import client.db.Channel;
+import client.db.Message;
+import client.db.User;
 import client.simplelogger.SimpleLogger;
 import client.simplelogger.SimpleLogger.LogLevel;
 
+@SuppressWarnings("all")
 public class ServerHandler implements Runnable {
 
     Socket server;
@@ -31,7 +37,6 @@ public class ServerHandler implements Runnable {
     @Override
     public void run() {
         try {
-            // TODO: implement sending commands to the server
             // LOGIN bWlya28ubGVvbi53ZWloQGxncy1odS5ldQ== bVdlMWhfMTIzNA==
 
             if (session.getState() != Session.State.DISCONNECTED) {
@@ -100,5 +105,70 @@ public class ServerHandler implements Runnable {
 
     public static void handle(String response) {
         
+    }
+
+    public String toBase64(String s) {
+        return Base64.getEncoder().encodeToString(s.getBytes());
+    }
+
+    // Commands
+
+	private void register(String emailAddress, String password, String nickname) {
+		sendCommand(String.format("REGISTER %s %s %s", toBase64(emailAddress), toBase64(password), toBase64(nickname)));
+    }
+
+	private void login(String emailAddress, String password) {
+        sendCommand(String.format("LOGIN %s %s", toBase64(emailAddress), toBase64(password)));
+    }
+
+    private Channel[] getPublicGroups() {
+		Channel[] publicGroups = null;
+		return publicGroups;
+    }
+
+    private void joinGroup(int channelID) {
+
+    }
+
+    private Channel[] getChannels() {
+		Channel[] channels = null;
+		return channels;
+    }
+
+    private User[] getChannelMembers(int channelID) {
+		User[] users = null;
+		return users;
+    }
+
+    private User getUser(int userID, String emailAddress) {
+		User user = null;
+		return user;
+    }
+
+    private void addFriend(int userID) {
+
+    }
+
+    private User[] getFriends() {
+		User[] users = null;
+		return users;
+    }
+
+    private void sendMessage(int channelID, Byte[] data, Enum dataType) {
+
+    }
+
+    private Channel createDM(int userID) {
+		Channel channel = null;
+		return channel;
+    }
+
+    private Message[] recieveMessages(int channelID, Timestamp tFrom, Timestamp tUntil) {
+		Message[] messages = null;
+		return messages;
+    }
+
+    private void quit() {
+        session.disconnect();
     }
 }
