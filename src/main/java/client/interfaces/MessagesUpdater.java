b9a3a38c990be9218f11bff11e009e0ba60dca68
dominik.hein <dominik.hein@lgs-hu.eu>
2022-01-25 06:01:31 +0100

    added concept of MessageUpdater

---
+++ src/main/java/client/interfaces/MessagesUpdater.java
@@ -0,0 +1,35 @@
+package client.interfaces;
+
+import client.ProtocolException;
+import client.db.Message;
+import client.db.User;
+
+import java.sql.Timestamp;
+
+//! sketch... not working just an idea
+public class MessagesUpdater implements Runnable {
+
+    User u;
+    Message m;
+
+    public MessagesUpdater(User u, Message m) {
+        this.u = u;
+        this.m = m;
+    }
+
+    @Override
+    public void run() {
+        int channelID = m.getChannel();
+        Timestamp tFrom = new Timestamp(System.currentTimeMillis() - 86400000); // - 1 day
+        Timestamp tUtil = new Timestamp(System.currentTimeMillis());
+
+        String response;
+        try {
+            response = GuiUpdateInterface.receiveMessages(channelID, tFrom, tUtil);
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: parse recieved Messages into Objects
+    }
+}
